70a9b846ec092dbe5c63ca432a7c4064ba05d34f
Jefferson <jefferson.dab@gmail.com>
2016-02-05 21:56:36 -0300

    Laying the base for the website LerManga

---
+++ src/MangaWebsite/LerManga.pm
@@ -0,0 +1,21 @@
+package MangaWebsite::LerManga;
+
+use strict;
+use warnings;
+
+use Helper;
+
+sub new {
+	my $class = shift;
+	return bless({}, $class);
+}
+
+sub getChapterList {
+
+}
+
+sub ripChapter {
+	
+}
+
+1;
